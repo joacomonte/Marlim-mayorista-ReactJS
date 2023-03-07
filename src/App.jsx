78e9b51b11bed8af51a11e14854c3f5ca47bd0df
Joaquin Montesano <montesano1994@gmail.com>
2023-03-07 13:52:11 -0300

    now to 800

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ export default function App() {
   // scrolls to the ref and then does a top margin correction
   function scrollWithOffset(argRef) {
     argRef.current.scrollIntoView();
-    setTimeout(()=>  {window.scrollBy(0, -100)}, 900)
+    setTimeout(()=>  {window.scrollBy(0, -100)}, 800)
   }
   
   useEffect(() => {
