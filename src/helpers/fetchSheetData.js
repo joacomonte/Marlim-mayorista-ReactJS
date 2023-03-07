b7cac5a800ee328116650321b9dec32afdaebc92
Joaquin Montesano <montesano1994@gmail.com>
2023-03-07 16:06:28 -0300

    update of fetchData

--- src/helpers/fetchSheetData.js
+++ src/helpers/fetchSheetData.js
@@ -1,11 +1,31 @@
-const fetchSheetData = async (sheetID, range, apiKey) => {
-    const response = await fetch(
-      `https://sheets.googleapis.com/v4/spreadsheets/${sheetID}/values/${range}?key=${apiKey}`
-    );
-    const data = await response.json();
-    console.log("has been fetched")
-    console.log(data)
-    return data;
+// const fetchSheetData = async (sheetID, range, apiKey) => {
+//     const response = await fetch(
+//       `https://sheets.googleapis.com/v4/spreadsheets/${sheetID}/values/${range}?key=${apiKey}`
+//     );
+//     const data = await response.json();
+//     console.log("FetchSheetData() has been called")
+//     return data;
+//   };
+  
+//   export default fetchSheetData;
+
+
+  const fetchSheetData = async (sheetId, range, apiKey) => {
+    console.log("FetchSheetData() has been called")
+
+    try {
+      const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${range}?key=${apiKey}`;
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Error fetching sheet data: ${response.status} ${response.statusText}`);
+      }
+      else {
+        const { values } = await response.json();
+        return values;
+      }
+    } 
+
+    catch (error) {throw new Error(`Error fetching sheet data: ${error.message}`);}
   };
   
   export default fetchSheetData;
