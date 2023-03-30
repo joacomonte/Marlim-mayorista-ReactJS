3455282c6b036906662632b1ccffcf55b6c8c7b9
Joaco <montesano1994@gmail.com>
2023-03-30 14:02:44 -0300

    error handling 2

--- src/helpers/fetchSheetData.js
+++ src/helpers/fetchSheetData.js
@@ -1,16 +1,17 @@
 const fetchSheetData = async (sheetId, range, apiKey) => {
+  const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${range}?key=${apiKey}`;
   try {
-    const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${range}?key=${apiKey}`
-    const response = await fetch(url)
+    const response = await fetch(url);
     if (!response.ok) {
-      console.log("Response not ok error")
-      throw new Error(await response.json())
+      console.error("Error occurred while fetching data");
+      throw new Error(await response.json());
     }
-    const data = await response.json()
-    return data
+    const data = await response.json();
+    return data;
   } catch (error) {
-    console.log("catch error: ", error.message)
+    console.error(`Error occurred while fetching data: ${error.message}`);
   }
-}
+};
 
-export default fetchSheetData
+export default fetchSheetData;
+
