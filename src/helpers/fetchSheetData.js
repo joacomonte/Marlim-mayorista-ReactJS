d8df5ad21a85049e79bb5451b2a4561165765f64
Joaco <montesano1994@gmail.com>
2023-03-30 14:01:39 -0300

    error handler

--- src/helpers/fetchSheetData.js
+++ src/helpers/fetchSheetData.js
@@ -1,21 +1,16 @@
-
-  const fetchSheetData = async (sheetId, range, apiKey) => {
-
-    try {
-      const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${range}?key=${apiKey}`;
-      const response = await fetch(url);
-      if (!response.ok) {
-        console.log("Response not ok error:", await response.json());
-      }
-      else {
-        console.log("Response ok: ", response);
-        const data = await response.json();
-        return data;
-      }
-    } 
-    catch (error) {
-      console.log("catch error: ", error.message);
+const fetchSheetData = async (sheetId, range, apiKey) => {
+  try {
+    const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${range}?key=${apiKey}`
+    const response = await fetch(url)
+    if (!response.ok) {
+      console.log("Response not ok error")
+      throw new Error(await response.json())
     }
-  };
-  
-  export default fetchSheetData;
+    const data = await response.json()
+    return data
+  } catch (error) {
+    console.log("catch error: ", error.message)
+  }
+}
+
+export default fetchSheetData
