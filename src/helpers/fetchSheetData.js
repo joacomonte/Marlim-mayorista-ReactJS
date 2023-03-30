5c5df74f113253092860d29f4a7c3cac6ac77711
Joaco <montesano1994@gmail.com>
2023-03-30 12:25:56 -0300

    error

--- src/helpers/fetchSheetData.js
+++ src/helpers/fetchSheetData.js
@@ -5,18 +5,16 @@
       const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${range}?key=${apiKey}`;
       const response = await fetch(url);
       if (!response.ok) {
-        console.log("res not ok: ",response);
+        console.log("Response not ok error:", await response.json());
       }
       else {
-        console.log("res ok",response)
-
+        console.log("Response ok: ", response);
         const data = await response.json();
         return data;
       }
     } 
-
     catch (error) {
-      console.log("catch error: ", error.messege)
+      console.log("catch error: ", error.message);
     }
   };
   
