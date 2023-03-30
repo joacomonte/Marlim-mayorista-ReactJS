c038d6b0ab23a20006a05bee6a0db39251914028
Joaco <montesano1994@gmail.com>
2023-03-30 14:12:41 -0300

    better practices

--- src/helpers/fetchSheetData.js
+++ src/helpers/fetchSheetData.js
@@ -1,16 +1,18 @@
-const fetchSheetData = async (sheetId, range, apiKey) => {
+async function fetchSheetData(sheetId, range, apiKey) {
   const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${range}?key=${apiKey}`;
+
   try {
     const response = await fetch(url);
+
     if (!response.ok) {
-      console.error('Error occurred while fetching data');
-      throw new Error(await response.json());
+      throw new Error(`Failed to fetch data from Google Sheets API. Status: ${response.status}.`);
     }
-    const data = await response.json();
-    return data;
+
+    return response.json();
   } catch (error) {
     console.error(`Error occurred while fetching data: ${error.message}`);
+    throw error;
   }
-};
+}
 
 export default fetchSheetData;
