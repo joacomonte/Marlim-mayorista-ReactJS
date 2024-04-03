4e59ed0f4d83b8fc6e6ddcab0ab348da6e8463c1
Joaco Montesano <montesano1994@gmail.com>
2024-04-03 17:49:30 -0300

    test

--- src/components/GenericCard.jsx
+++ src/components/GenericCard.jsx
@@ -32,26 +32,27 @@ const GenericCard = ({ cell }, ref) => {
 
   return (
     <div ref={ref} className='genericCard' name='toHere'>
-      {title && <h1>{title}</h1>}
+      <div>
+        {title && <h1>{title}</h1>}
 
-      {img && img.length > 1 && (
-        <Swiper pagination={true} modules={[Pagination]} className='mySwiper'>
-          {img.map((src, index) => (
-            <SwiperSlide key={index}>
-              <div className='genericCard__imgContainer' onClick={() => setIsClicked(!isClicked)}>
-                {src ? <img src={`/${src}`} alt='img'></img> : <p>Imagen no disponible</p>}
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      )}
+        {img && img.length > 1 && (
+          <Swiper pagination={true} modules={[Pagination]} className='mySwiper'>
+            {img.map((src, index) => (
+              <SwiperSlide key={index}>
+                <div className='genericCard__imgContainer' onClick={() => setIsClicked(!isClicked)}>
+                  {src ? <img src={`/${src}`} alt='img'></img> : <p>Imagen no disponible</p>}
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        )}
 
-      {img && img.length === 1 && (
-        <div className='genericCard__imgContainer' onClick={() => setIsClicked(!isClicked)}>
-          {img[0] ? <img src={`/${img[0]}`} alt='img'></img> : <p>Imagen no disponible</p>}
-        </div>
-      )}
-
+        {img && img.length === 1 && (
+          <div className='genericCard__imgContainer' onClick={() => setIsClicked(!isClicked)}>
+            {img[0] ? <img src={`/${img[0]}`} alt='img'></img> : <p>Imagen no disponible</p>}
+          </div>
+        )}
+      </div>
       <div className='genericCard__description'>
         {subtitle1 && <h2>{subtitle1}</h2>}
         {modelo1 && precio1 && (
