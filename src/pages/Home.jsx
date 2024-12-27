204457eec09a030a2193b09c4f19fe97fad80c11
marlim23 <127150428+marlim23@users.noreply.github.com>
2024-12-27 18:35:08 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -79,9 +79,9 @@ export default function Home() {
     [['inox.jpg'], inoxiRef], // Sorbetes de acero inoxidable
     [['IMG_0727.png'], bombillasRef], // Bombillas Aluminio con Resorte
     [['']], // Bombillas Chata Aluminio Anodizado
-    [['IMG_8216.jpg', 'IMG_8211.jpg', 'IMG_8206.jpg', 'vaca.jpg'], vasoRef], // Vaso Inox y Sorbete 21cm Diseños
-    [['vaso waterdog negro.jpg', 'vaso waterdog arena.jpg', 'vaso waterdog blanco.jpg', 'vaso waterdog verde.png']], // Vaso Inox y Sorbete 21cm (Personalizable)
-    [['IMG_8002.jpg', 'mn.jpg'], mateRef], // Mate Inox y Bombi 15cm Diseños
+    [['IMG_8211.jpg'], vasoRef], // Vaso Inox y Sorbete 21cm Diseños
+    [['vaso waterdog negro.jpg', 'vaso waterdog blanco.jpg', 'vaso waterdog verde.png']], // Vaso Inox y Sorbete 21cm (Personalizable)
+    ['mn.jpg'], mateRef], // Mate Inox y Bombi 15cm Diseños
     [['IMG_20221220_203943912.jpg']], // Mate Inox y Bombi 15cm (Personalizable)
     [['img_8406.jpg'], limpiaCerdaRef], // Limpiador de cerda
     [['est.jpg', 'est pers.jpg'], estucheRef], // Estuche de viaje - Cartón compacto
